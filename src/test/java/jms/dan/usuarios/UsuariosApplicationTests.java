94c86b14dba1240a2c2451e8f182e3dea9c89af7
MalenaMoix <moixmalena@gmail.com>
2021-03-28 16:51:32 -0300

    Initial commit

---
+++ src/test/java/jms/dan/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package jms.dan.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
