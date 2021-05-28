70f8573620e65dad6bdcb772e971ec08ed93e0d6
Santiago Cobelli <cobellisantiago@gmail.com>
2021-05-28 01:37:47 -0300

    Add data base initialization data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_type (id, type)
+VALUES (1, 'Cliente'), (2, 'Empleado');
+INSERT INTO construction_type (id, description)
+VALUES (1, 'Reforma'), (2, 'Casa'), (3, 'Edificio'), (4, 'Vial');
